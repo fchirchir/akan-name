6e20389a0e9e58144d3c341ac1664691d11d3c9c
Felix <felixchirchir@gmail.com>
2020-05-31 23:13:19 +0300

    Add calculator formula

--- js/scripts.js
+++ js/scripts.js
@@ -46,4 +46,31 @@ function getAkanName () {
       //validation variables
       let monthValid = monthValidator();
       let dayValid = dayValidator();
-      
+
+      //formula to determine day of birth (Sunday = 1, Monday = 2)etc..
+    let dayOfWeekNumber = Math.floor((((Number(yearOfBirth.slice(0,2))/4)-2*Number(yearOfBirth.slice(0,2))-1)+
+    ((5*Number(yearOfBirth.slice(2,4))/4))+((26*(monthOfBirth+1)/10))+dayOfBirth)%7);
+
+//creating arrays of Akan names for males & females and days of the week
+let daysOfWeek = [
+"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+];
+
+let maleAkanNames = [
+"Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"
+];
+
+let femaleAkanNames = [
+"Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"
+];
+
+//generating and index value to select items on arrays
+let index;
+// fix formula bug
+if (dayOfWeekNumber == 0){
+index = 6;
+} else {
+index = dayOfWeekNumber - 1;
+}
+
+console.log(index);
