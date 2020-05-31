38a7337977b18f301bfe707439927ba3b43e144d
Felix <felixchirchir@gmail.com>
2020-05-31 23:07:36 +0300

    Add initial functions and variables

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function getAkanName () {
+    let yearOfBirth = document.getElementById("year-input").value;
+    let monthOfBirth = Number(document.getElementById("month-input").value);
+    let dayOfBirth = Number(document.getElementById("day-input").value);
+    let genders = document.getElementsByName("gender");
+
+    // function to get gender
+    function getGender () {
+        for (let gender of genders){
+          if (gender.checked){
+            return gender.value;
+          }
+        }
+      }
